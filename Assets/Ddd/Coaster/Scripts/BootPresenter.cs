e888330f08400bec9926309c26681172e45d0251
StickeyFingersESW9 <koiwai.1059373.me03sf.ilohas@gmail.com>
2018-04-30 11:53:14 +0900

    namespaceの追加と整形

--- Assets/Ddd/Coaster/Scripts/BootPresenter.cs
+++ Assets/Ddd/Coaster/Scripts/BootPresenter.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BootPresenter : MonoBehaviour {
+namespace Ddd.Coaster
+{
+	public class BootPresenter : MonoBehaviour
+	{
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start()
+		{
+		}
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		// Update is called once per frame
+		void Update()
+		{
+		}
 	}
 }
